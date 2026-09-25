3330242a5f367837c6b6f8f897ff570c2110b32c
SS0391 <ss0391web@gmail.com>
2026-09-25 13:41:31 +0200

    startet over, made to many error in the beginning

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
